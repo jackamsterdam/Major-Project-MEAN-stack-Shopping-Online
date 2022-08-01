0e47ec5e8ac6d3a21955f4e4940d5de5f55c327f
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-08-01 23:19:29 +0300

    fixed case where categories where still showing by setting categories to all in logout

--- Frontend/src/app/components/auth-area/logout/logout.component.ts
+++ Frontend/src/app/components/auth-area/logout/logout.component.ts
@@ -1,3 +1,4 @@
+import { ProductsService } from './../../../services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
@@ -8,10 +9,11 @@ import { NotifyService } from 'src/app/services/notify.service';
   template: ""
 })
 export class LogoutComponent implements OnInit {
-
-  constructor(private authService: AuthService, private notify: NotifyService, private router: Router) { }
+searchText: string
+  constructor(private authService: AuthService, private notify: NotifyService, private router: Router, private productsService: ProductsService) { }
 
   ngOnInit() {
+  this.productsService.setSelectedCategory('all')
   this.authService.logout()
   this.notify.success('You are logged-out')
   this.router.navigateByUrl('/home') 
