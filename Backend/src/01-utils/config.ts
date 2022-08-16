170a52b180451feacdde04a181cf7c288123a8b0
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-08-16 22:32:45 +0300

    connection string

--- Backend/src/01-utils/config.ts
+++ Backend/src/01-utils/config.ts
@@ -11,7 +11,7 @@ class DevelopmentConfig extends Config {
 class ProductionConfig extends Config {
     isDevelopment = false
     logFile = 'logger.log'
-    connectionString = 'mongodb://localhost:27017/ShoppingOnlineDB'
+    connectionString = 'mongodb+srv://jackamsterdam:aGnKub8j34m4xg0p@mean-supermarket-by-jac.6msdj3y.mongodb.net/?retryWrites=true&w=majority'
 }
 
 const config = process.env.NODE_ENV === 'production' ? new ProductionConfig() : new DevelopmentConfig()
